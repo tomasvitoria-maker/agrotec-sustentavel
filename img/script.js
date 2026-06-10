70570220d56ffea4cb27df3b4247423a0ba954d3
tomasvitoria-maker <tomas.vitoria@escola.pr.gov.br>
2026-06-10 14:35:32 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,1 +1,19 @@
+function exibirInfo(){
+    alert("O Campo Verde promove inovação, produtividade e preservação ambiental.");
+}
+
+function calcular(){
+    let hectares = Number(document.getElementById("hectares").value);
+
+    if(hectares <= 0){
+        document.getElementById("resultado").innerHTML =
+        "Digite um valor válido.";
+        return;
+    }
+
+    let arvores = hectares * 500;
+
+    document.getElementById("resultado").innerHTML =
+    `Impacto estimado: ${arvores} árvores preservadas.`;
+}
 
