fbce1fb3eab4b9bad6992e4cd7bf73d7e381b713
tomasvitoria-maker <tomas.vitoria@escola.pr.gov.br>
2026-06-10 15:07:49 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,1 +1,25 @@
+const chuva = document.getElementById("chuva");
+const valorChuva = document.getElementById("valorChuva");
 
+chuva.addEventListener("input", () => {
+    valorChuva.textContent = chuva.value;
+});
+
+function captarAgua(){
+
+    let mm = parseInt(chuva.value);
+
+    let volume = mm * 10;
+
+    let porcentagem = mm;
+
+    if(porcentagem > 100){
+        porcentagem = 100;
+    }
+
+    document.getElementById("nivelAgua").style.height =
+        porcentagem + "%";
+
+    document.getElementById("resultado").innerHTML =
+        `💧 Foram armazenados aproximadamente <strong>${volume} litros</strong> de água da chuva para irrigação sustentável.`;
+}
